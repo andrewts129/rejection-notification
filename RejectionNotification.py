bc1f46b115798d47323a77b94791562403f69500
Andrew Smith <andrewts129@gmail.com>
2018-02-23 22:15:12 -0500

    inital functionality complete

--- RejectionNotification.py
+++ RejectionNotification.py
@@ -1,11 +1,11 @@
-from O365 import Inbox, Message
+from O365 import Inbox
 import os
 import subprocess
 import datetime
 
 
 def is_rejection(subject, body):
-    full_text = (subject + body).lower
+    full_text = (subject + body).lower()
 
     # Needs to have one of these
     required_words = ["intern", "internship"]
@@ -16,10 +16,7 @@ def is_rejection(subject, body):
                          "another applicant", "for your interest"]
     troublesome_counts = [full_text.count(x) for x in troublesome_words]
 
-    if sum(required_counts) >= 1 and sum(troublesome_counts) >= 2:
-        return True
-    else:
-        return False
+    return sum(required_counts) >= 1 and sum(troublesome_counts) >= 2
 
 
 def play_taps():
@@ -28,15 +25,19 @@ def play_taps():
     subprocess.call(["mpg321", "Taps.mp3"])
 
 
-auth = (os.environ["EMAIL_ADDRESS"], os.environ["PASSWORD"])
-inbox = Inbox(auth=auth, getNow=False)
+def main():
+    auth = (os.environ["EMAIL_ADDRESS"], os.environ["PASSWORD"])
+    inbox = Inbox(auth=auth, getNow=False)
 
-# Gets all messages received in the last two minutes
-two_minutes_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=2)
-inbox.setFilter("DateTimeReceived gt " + two_minutes_ago.strftime("%Y-%m-%dT%H:%M:%SZ"))
-inbox.getMessages()
+    # Gets all messages received in the last two minutes
+    two_minutes_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=2)
+    inbox.setFilter("DateTimeReceived gt " + two_minutes_ago.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    inbox.getMessages()
 
-for message in inbox.messages:
-    print(message)
+    for message in inbox.messages:
+        if is_rejection(message.getSubject(), message.getBody()):
+            play_taps()
 
 
+if __name__ == "__main__":
+    main()
