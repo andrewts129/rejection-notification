44a9246613c5c4c13e215bb27f518e7aba0082ce
Andrew Smith <andrewts129@gmail.com>
2018-08-21 17:02:54 -0400

    model tweaking and exporting

--- Training.py
+++ Training.py
@@ -4,10 +4,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from imblearn.over_sampling import SMOTE
+from sklearn.externals import joblib
 
 
 with open("emails-labelled.json", "r") as file:
@@ -34,15 +35,19 @@ labels = np.asarray([int(email["isRejection"]) for email in data])
 
 train, test, train_labels, test_labels = train_test_split(features, labels, test_size=0.20, random_state=109)
 
-train, train_labels = SMOTE(random_state=109).fit_sample(train, train_labels)
+#train, train_labels = SMOTE(random_state=109).fit_sample(train, train_labels)
 
-algo = LogisticRegression(class_weight="balanced")
-#algo = svm.SVC(kernel="linear", class_weight="balanced")
-#algo = RandomForestClassifier(class_weight="balanced")
-#algo = GradientBoostingClassifier()
-model = algo.fit(train, train_labels)
+model = LogisticRegression(class_weight="balanced")
+#model = svm.SVC(kernel="linear", class_weight="balanced")
+#model = RandomForestClassifier(class_weight="balanced")
+#model = GradientBoostingClassifier()
+model.fit(train, train_labels)
 
-predictions = algo.predict(test)
+# Adjusts the intercept because the data is heavily biased (way more non-rejections than rejections)
+prior = 0.57
+model.intercept_ += np.log(prior / (1 - prior)) - np.log((1 - prior) / prior)
+
+predictions = model.predict(test)
 
 cm = confusion_matrix(test_labels, predictions)
 
@@ -52,3 +57,7 @@ print("True Positives: " + str(cm[1][1]) + " (" + format(cm[1][1] / len(predicti
 print("False Positives: " + str(cm[0][1]) + " (" + format(cm[0][1] / len(predictions) * 100, ".2f") + "%)")
 print()
 print("Rejections Correct: " + format(cm[1][1] / (cm[1][0] + cm[1][1]) * 100, ".2f") + "%")
+print()
+print("ROC AUC Score: " + str(roc_auc_score(test_labels, predictions)))
+
+joblib.dump(model, "model.pkl", compress=9)
