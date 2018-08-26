a5e12248d10ca032d86a8c1371fd6718115c987a
Andrew Smith <andrewts129@gmail.com>
2018-08-26 18:24:45 -0400

    actually using all available data for training

--- RejectionNotification.py
+++ RejectionNotification.py
@@ -3,31 +3,109 @@ import os
 import subprocess
 import datetime
 import logging
+import schedule
+import time
+from sklearn.externals import joblib
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import json
+import json
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn.externals import joblib
+
 
 logging.basicConfig(filename="logs/rejection-notification.log", format='%(asctime)s %(message)s', level=logging.INFO)
 
 
-def is_rejection(subject, body):
-    full_text = (subject + body).lower()
+def train_model():
+    with open("emails-labelled.json", "r") as file:
+        data = json.load(file)
 
-    # Needs to have one of these
-    required_words = ["intern", "internship"]
-    required_counts = [full_text.count(x) for x in required_words]
+    subject_vectorizer = TfidfVectorizer()
+    subject_vectorizer.fit([email["subject"] for email in data])
+    body_vectorizer = TfidfVectorizer()
+    body_vectorizer.fit([email["bodyText"] for email in data])
+    sender_name_vectorizer = TfidfVectorizer()
+    sender_name_vectorizer.fit([email["senderName"] for email in data])
 
-    # Should have at least a few of these
-    troublesome_words = ["unfortunately", "other candidates", "other applicants", "another candidate",
-                         "another applicant", "your interest", "not be moving forward",
-                         "unable to move forward", "not to move forward", "not selected for this position",
-                         "unable to offer you a position at this time", "has been filled", "wish you the best of luck",
-                         "was not selected", "carefully reviewed your", "unable to move forward"]
-    troublesome_counts = [full_text.count(x) for x in troublesome_words]
+    subject_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["subject"] for email in data]).A])
+    body_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["bodyText"] for email in data]).A])
+    sender_name_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["senderName"] for email in data]).A])
 
-    return sum(required_counts) >= 1 and sum(troublesome_counts) >= 2
+    features = np.asarray([subject_feature, body_feature, sender_name_feature])
+
+    # Needed bc scikit's fit function only accepts 2D arrays
+    features = features.reshape(len(data), -1)
+
+    labels = np.asarray([int(email["isRejection"]) for email in data])
+
+    model = LogisticRegression(class_weight="balanced")
+    model.fit(features, labels)
+
+    # Adjusts the intercept because the data is heavily biased (way more non-rejections than rejections)
+    prior = 0.57
+    model.intercept_ += np.log(prior / (1 - prior)) - np.log((1 - prior) / prior)
+
+    return model, subject_vectorizer, body_vectorizer, sender_name_vectorizer
 
 
-def play_taps():
-    if not os.path.isfile("Taps.mp3"):
-        subprocess.call(["wget", "-O", "Taps.mp3", "http://www.music.army.mil/music/buglecalls/play.asp?Taps.mp3"])
+def check_for_rejections(inbox, model, subject_vectorizer, body_vectorizer, sender_name_vectorizer):
+    def get_messages(inbox):
+        def build_email_dict(message_obj):
+            def text_from_html(body):
+                # https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
+                def tag_visible(element):
+                    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+                        return False
+                    if isinstance(element, Comment):
+                        return False
+                    return True
+
+                soup = BeautifulSoup(body, 'html.parser')
+                texts = soup.findAll(text=True)
+                visible_texts = filter(tag_visible, texts)
+                return u" ".join(t.strip() for t in visible_texts)
+
+            return {"senderName": message_obj.getSenderName() if message_obj.getSenderName() is not None else "",
+                    "subject": message_obj.getSubject() if message_obj.getSubject() is not None else "",
+                    "body": text_from_html(message_obj.getBody()) if message_obj.getBody() is not None else ""}
+
+        # Gets all messages received in the last minute
+        one_minute_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=1, seconds=1)
+        inbox.setFilter("DateTimeReceived gt " + one_minute_ago.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        inbox.getMessages()
+
+        return [build_email_dict(message) for message in inbox.messages]
+
+    messages = get_messages(inbox)
+
+    subject_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["subject"] for email in messages]).A])
+    body_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["bodyText"] for email in messages]).A])
+    sender_name_feature = np.asarray(
+        [row for row in subject_vectorizer.transform([email["senderName"] for email in messages]).A])
+
+    features = np.asarray([subject_feature, body_feature, sender_name_feature])
+
+    predictions = model.predict(features)
+
+    if sum(predictions) > 0:
+        play_music()
+
+
+def play_music():
     subprocess.call(["mpg321", "Taps.mp3"])
 
 
@@ -35,21 +113,13 @@ def main():
     auth = (os.environ["EMAIL_ADDRESS"], os.environ["PASSWORD"])
     inbox = Inbox(auth=auth, getNow=False)
 
-    # Gets all messages received in the last two minutes
-    # Gives an extra 10 seconds for startup time
-    two_minutes_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=2, seconds=10)
-    inbox.setFilter("DateTimeReceived gt " + two_minutes_ago.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    inbox.getMessages()
+    messages =
+    model, subject_vectorizer, body_vectorizer, sender_name_vectorizer = train_model(messages)
+    schedule.every(1).minutes.do(check_for_rejections, inbox, model, subject_vectorizer, body_vectorizer, sender_name_vectorizer)
 
-    logging.info("Found " + str(len(inbox.messages)) + " messages")
-
-    for message in inbox.messages:
-        logging.info("Found message with subject: " + message.getSubject())
-        if is_rejection(message.getSubject(), message.getBody()):
-            logging.info("Rejected!")
-            play_taps()
-        else:
-            logging.info("Not rejected...")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
--- Training.py
+++ Training.py
@@ -14,6 +14,8 @@ from sklearn.externals import joblib
 with open("emails-labelled.json", "r") as file:
     data = json.load(file)
 
+print("Vectorizing...")
+
 subject_vectorizer = TfidfVectorizer()
 subject_vectorizer.fit([email["subject"] for email in data])
 body_vectorizer = TfidfVectorizer()
@@ -21,22 +23,33 @@ body_vectorizer.fit([email["bodyText"] for email in data])
 sender_name_vectorizer = TfidfVectorizer()
 sender_name_vectorizer.fit([email["senderName"] for email in data])
 
-
+body_feature = np.asarray([row for row in body_vectorizer.transform([email["bodyText"] for email in data]).A])
 subject_feature = np.asarray([row for row in subject_vectorizer.transform([email["subject"] for email in data]).A])
-body_feature = np.asarray([row for row in subject_vectorizer.transform([email["bodyText"] for email in data]).A])
-sender_name_feature = np.asarray([row for row in subject_vectorizer.transform([email["senderName"] for email in data]).A])
+sender_name_feature = np.asarray([row for row in sender_name_vectorizer.transform([email["senderName"] for email in data]).A])
 
-features = np.asarray([subject_feature, body_feature, sender_name_feature])
+print("About to shape...")
+skeleton = np.zeros(body_feature.shape)
+skeleton[:subject_feature.shape[0], :subject_feature.shape[1]] = subject_feature
+subject_feature = skeleton
+
+skeleton = np.zeros(body_feature.shape)
+skeleton[:sender_name_feature.shape[0], :sender_name_feature.shape[1]] = sender_name_feature
+sender_name_feature = skeleton
+
+features = np.asarray([body_feature, subject_feature, sender_name_feature])
 
 # Needed bc scikit's fit function only accepts 2D arrays
 features = features.reshape(len(data), -1)
 
 labels = np.asarray([int(email["isRejection"]) for email in data])
 
+print("Splitting...")
 train, test, train_labels, test_labels = train_test_split(features, labels, test_size=0.20, random_state=109)
 
+#print("SMOTING...")
 #train, train_labels = SMOTE(random_state=109).fit_sample(train, train_labels)
 
+print("About to train...")
 model = LogisticRegression(class_weight="balanced")
 #model = svm.SVC(kernel="linear", class_weight="balanced")
 #model = RandomForestClassifier(class_weight="balanced")
@@ -44,9 +57,10 @@ model = LogisticRegression(class_weight="balanced")
 model.fit(train, train_labels)
 
 # Adjusts the intercept because the data is heavily biased (way more non-rejections than rejections)
-prior = 0.57
-model.intercept_ += np.log(prior / (1 - prior)) - np.log((1 - prior) / prior)
+#prior = 0.54
+#model.intercept_ += np.log(prior / (1 - prior)) - np.log((1 - prior) / prior)
 
+print("Predicting...")
 predictions = model.predict(test)
 
 cm = confusion_matrix(test_labels, predictions)
@@ -60,4 +74,5 @@ print("Rejections Correct: " + format(cm[1][1] / (cm[1][0] + cm[1][1]) * 100, ".
 print()
 print("ROC AUC Score: " + str(roc_auc_score(test_labels, predictions)))
 
+print("Dumping...")
 joblib.dump(model, "model.pkl", compress=9)
